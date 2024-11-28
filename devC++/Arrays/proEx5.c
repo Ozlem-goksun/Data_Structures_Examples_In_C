a7777b0f96a52946be10d3fdae70182221824c0e
dinosOnVenus <ozlemgoksun1389@gmail.com>
2024-11-29 01:41:15 +0300

    proex 5 6 7 8

--- devC++/Arrays/proEx5.c
+++ devC++/Arrays/proEx5.c
@@ -1,13 +1,30 @@
 // Write a program to enter "n" number of digits.Form a number using these digits.
 
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-	int nums, digit[0], num_of_digits, i;
+	int num = 0, digit[10], num_of_digits, i;
 	
 	printf("\n Enter the number of digits : ");
 	scanf("\n %d", &num_of_digits);
 	
+	for (i = 0; i < num_of_digits; i++)
+	{
+		printf("\n Enter the digit at position %d : ", i + 1);
+		scanf("%d", &digit[i]);
+	}
 	
+	i = 0;
+	
+	while (i < num_of_digits)
+	{
+		num = num + digit[i] * pow(10, i);
+		i++;
+	}
+	
+	printf("\n The number is : %d", num);
+	
+	return 0;	
 }
