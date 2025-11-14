a541e74da1f52a5b2034fdbf11f2ed4c27daa91c
dinosOnVenus <ozlemgoksun1389@gmail.com>
2025-11-14 21:21:30 +0300

    .

--- devC++/Introduction to C/DENEME_TAHTASI.c
+++ devC++/Introduction to C/DENEME_TAHTASI.c
@@ -2,17 +2,35 @@
 
 int main()
 {
-	int i, n, nums[20], sum = 0;
+	int i, n, n1, arr[n], pos;
 	
-	printf("Toplanacak eleman sayisini giriniz : ");
+	printf("diziniz kaç elemanli: ");
 	scanf("%d", &n);
 	
-	for (i = 0; i < n; i++)
+	for(i = 0; i < n; i++)
 	{
-		printf("%d. degerinizi giriniz : ", i + 1);
-		scanf("%d", &nums[i]);
-		sum += nums[i];
+		printf("%d. eleman: ", i+1);
+		scanf("%d", &arr[i]);
 	}
 	
-	printf("%d", sum);
+	printf("diziye eklenecek yeni eleman: ");
+	scanf("%d", &n1);
+	printf("eklenecek elemanin pozisyonu: ");
+	scanf("%d", &pos);
+	
+	n = n + 1;
+	
+	arr[pos] = n1;
+	
+	for(i = n ; i >= pos; i--)
+	{
+		arr[i] = arr[i-1];
+	}
+	
+	printf("dizinin son hali: \n");
+	
+	for(i = 0; i < n; i++)
+	{
+		printf("arr[%d] = %d \n", i, arr[i]);
+	}
 }
