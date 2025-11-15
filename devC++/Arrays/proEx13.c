9b990e68cba94d8318f1b6a6b817267244e19511
dinosOnVenus <ozlemgoksun1389@gmail.com>
2025-11-15 16:43:30 +0300

    .

---
+++ devC++/Arrays/proEx13.c
@@ -0,0 +1,3 @@
+#include<stdio.h>
+
+// Write a program to read an array of n numbers and then find the smallest number.
--- devC++/Introduction to C/DENEME_TAHTASI.c
+++ devC++/Introduction to C/DENEME_TAHTASI.c
@@ -1,35 +1,14 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main()
 {
-	int i, arr1[3], arr2[3], arrM[6];
-	
-	printf("ilk dizinin elemanlarını giriniz: \n");
-	
-	
-	for(i = 0; i < 3; i++)
-	{
-		printf("%d. eleman: ", i+1);
-		scanf("%d", &arr1[i]);
-		arrM[i] = arr1[i];
-	}
-	
-	printf(" ikinci dizinin elemanlarını giriniz: \n");
-	
-	for(i = 0; i < 3; i++)
-	{
-		printf("%d. eleman: ", i + 1);
-		scanf("%d", &arr2[i]);
-		arrM[i+3] = arr2[i];
-	}
-	
-	printf("iki dizinin birleştirilmiş hali:\n");
-	
-	for(i = 0; i < 6; i++)
-	{
-		printf("%d. eleman: %d \n", i + 1, arrM[i]);
-	}
+	int age = 50;
+	int *pAge = &age;
 	
 	
+	printf("value of age: %d\n", age);
+	printf("address of age: %p\n", &age);
 	
+	printf("value of pAge : %p\n", pAge);
+	printf("value at stored address: %d", *pAge);
 }
